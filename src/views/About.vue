4bc7aa09eb06e6dc00b602e0a824ce06a4153bcb
Aline <aline.isilvadamiao@gmail.com>
2019-10-19 14:44:17 -0300

    página sobre
    página sobre

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,56 @@
 <template>
   <div>
-    <v-container>
-      <v-row>
-        <v-col
-        cols="12"
-        md="4"
-        >
-        <h3 class="titulo-section">{Sobre nós}</h3>
-        <hr class="border">
-        <p>
-          É importante lembrar de onde viemos para saber aonde queremos chegar. Antes de nos unirmos para formar uma das maiores empresa de desenvolviemnto na área da tecnologia do Brasil, tivemos uma história cheia de desafios e de conquistas que nos trouxeram até aqui. Conheça os marcos mais importantes da nossa história!
-        </p>
-        </v-col>
-        <v-col
-        cols="12"
-        md="8"
-        class="d-flex"
-        >
-        <div class="iframe-container">
-          <iframe width="" height="" src="" frameborder="0"> </iframe>
-        </div>
+   <body>
+    <section class="sobre-section">
+        <div class="interior-width">
+            <h1>Sobre nós</h1>
+            <hr class="border">
+            <div class="sobre-section-linha">
+                <div class="sobre-section-coluna">
+                    <div class="sobre">
+                        <p>É importante lembrar de onde viemos para saber aonde queremos chegar. Antes de nos unirmos
+                            para formar uma das maiores empresas de desenvolvimento na área da tecnologia, tivemos uma
+                            história cheia de desafios e de conquistas, as quais nos trouxeram até aqui. Conheça os
+                            marcos mais importantes da nossa história!
+                        </p>
+                       
+                    </div>
 
-        </v-col>
-      </v-row>
-    </v-container>
+                </div>
+
+                <div class="sobre-section-coluna">
+
+                    <div class="video">
+                        <div class="embed">
+                                <iframe src="https://www.youtube.com/embed/MeWET6I-Jd4"></iframe>
+                        </div>
+                    </div>
+
+                    <!-- <div class="skills">
+                        <div class="skill">
+                            <div class="title"> Web Design</div>
+                            <div class="progress">
+                                <div class="progress-bar p2">
+                                    <span>70%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+
+                    <!-- <div class="skills">
+                        <div class="skill">
+                            <div class="title"> Web Design</div>
+                            <div class="progress">
+                                <div class="progress-bar p3">
+                                    <span>50%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
   </div>
 </template>
