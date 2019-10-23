a5e9344b8840b418a2290b62d6dc362832d3e36a
Aline <aline.isilvadamiao@gmail.com>
2019-10-23 12:56:56 -0300

    index

--- src/views/painel/Index.vue
+++ src/views/painel/Index.vue
@@ -1,85 +1,46 @@
 <template>
 <div>
   <!-- card -->
-  <v-card>
+  <template>
+  <v-card height="350px">
     <v-navigation-drawer
-      expand-on-hover
+      absolute
       permanent
-      relative
+      left
     >
-    <!-- dentro do card -->
       <template v-slot:prepend>
-        <!-- lista -->
-        <v-list>
-          <!-- itens da lista -->
-          <v-list-item>
-            <!-- intens da lista: avatar -->
-            <v-list-item-avatar>
-              <!-- avatar tem a cor amarela -->
-              <v-avatar color="red">
-                <!-- dentro dessa cor há um escrito em branco "AD" -->
-                <span class="white--text headline">AL</span>
-              </v-avatar>
-            </v-list-item-avatar>
-          </v-list-item>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg">
+          </v-list-item-avatar>
 
-          <v-list-item
-            link
-            two-line
-          >
-            <v-list-item-content>
-              <!-- título da lista -->
-              <v-list-item-title class="title">Bem-vindo de volta!</v-list-item-title>
-              <!-- subtítulo -->
-              <!-- nome que aparece no login -->
-              <v-list-item-subtitle>{{ user.nome }}</v-list-item-subtitle>
-            </v-list-item-content>
-            <!-- ações da lista -->
-            <v-list-item-action>
-              <!-- [icone] -->
-              <v-icon>mdi-menu-down</v-icon>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-        
+          <v-list-item-content class="">
+            <v-list-item-title>oi</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </template>
 
       <v-divider></v-divider>
 
-      <v-list
-        nav
-        dense
-      >
-      <!-- item da lista com link -->
-        <v-list-item link>
-          <!-- ícone -->
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+        
+        >
           <v-list-item-icon>
-            <v-icon @click="manut" color="black">mdi-rocket</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
-          <!-- escrito ao lado do ícone, um título -->
-          <v-list-item-title @click="manut">Inscritos Hackathon</v-list-item-title>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-icon>
-            <!-- ícone com links -->
-            <v-icon @click="usuario" color="black">mdi-account-multiple</v-icon>
-          </v-list-item-icon>
-          <!-- o @click é um evento, dentro dele há o método que é o caminho aonde ele irá  percorrer -->
-          <v-list-item-title @click="usuario">Administradores</v-list-item-title>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-icon>
-            <v-icon @click="sair" color="black">mdi-location-exit</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title @click="sair">Sair</v-list-item-title>
-        </v-list-item>
 
-  
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
-  
- 
   </v-card>
+</template>
 
   
  
@@ -93,30 +54,15 @@
 
 </style>
 <script>
-export default {
-   computed: {
-    user () {
-      return this.$ls.get('user')
-    }
-  },
-// o método sair irá em router e chamará pelo caminho /painel/login e assim por diante
-  methods:{
-    sair(){
-      this.$router.push('/painel/login')
-    },
-    usuario(){
-      this.$router.push('/painel/usuarios')
-    },
-    manut(){
-      this.$router.push('/painel/inscritos')
-    }
-
-  },
-   data () {
+  export default {
+    data () {
       return {
-        alert: true,
+        items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
       }
     },
-}
+  }
 </script>
-
